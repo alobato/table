6e4d0b3e6060b0918fcf8aacfd4f5593961b05c8
Arley Lobato <arleylobato@gmail.com>
2019-08-08 01:06:06 +0100

    remove macro

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components/macro'
+import styled from 'styled-components'
 import { width, height, lineHeight } from 'styled-system'
 
 export const TableContainer = styled.div`
